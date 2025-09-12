2790a30d3c560d123092c71b25d6ebf070b62929
Muzammil <@muzammil6686>
2025-09-12 09:26:36 +0530

    first commit

---
+++ new.py
@@ -0,0 +1,60 @@
+while True:
+    tem=int(input("temperature de do bhai sahab "))
+    if tem < 15:
+        print("dil garden garden ho raha hai mousam bahot hi zyada thanda hai")
+    elif tem >= 15 and tem <= 30:
+        print("chup chap raho mousam bahot hi mast  hai")
+    else:
+        print("ghar se bahar mat nikalna jal jaoge or kkale ho jaoge")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# while True:
+#     weight=float(input("enter a weight :"))
+#     height=float(input("enter a height :"))
+#     height=height*0.3048
+#     bmi=weight/(height*height)
+#     print("Bmi =",bmi)
+#     if bmi < 18.5:
+#         print("under weight")
+#     elif bmi > 18.5 and bmi < 24.9:
+#         print("healthy")
+#     elif bmi > 25.0 and bmi < 29.9:
+#         print("over weight")
+#     else:
+#         print("meet nutritionist")
+
+
+
+
+
+
+
+#
+# A healthy adult BMI range is generally 18.5 to 24.9. A BMI below 18.5 is considered underweight,
+# while a BMI of 25.0 to 29.9 indicates overweight,
+# and a BMI of 30.0 or higher is classified as obesity.
