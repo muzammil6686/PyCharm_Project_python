5ab5654ee792c0c5130a41777a07a65a2fd7ee61
Muzammil <@muzammil6686>
2025-09-13 13:38:14 +0530

    first change

---
+++ level 2.py
@@ -0,0 +1,12 @@
+
+income=int(input("income de do"))
+how=int(input("kitne exp hai "))
+
+dict={}
+
+
+for i in range(how):
+    input("enter exp no ",i)
+    input("enter cost of item ",i)
+
+    print(i)
